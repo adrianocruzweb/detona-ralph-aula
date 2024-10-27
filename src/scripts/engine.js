ffcc4a7340bd26805a7b4827a930002b2b5d412f
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-27 15:09:26 -0400

    new details audios and img

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,15 +5,52 @@ const state = {
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score")
     },
-    values: {}
+    values: {
+        timerId: null,
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+    }
 };
 
-function addListenerHitBox() {
+function randomSquare() {
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    });
 
+    let randomNumber = Math.floor(Math.random()*9);
+
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume(0.2);
+    audio.play();
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square)=>{
+        square.addEventListener("mousedown",()=>{
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound('hit');
+            }
+        });
+    });
 }
 
 function initialize() {
-
+    moveEnemy();
+    addListenerHitBox();
 }
 
 initialize();
