b19b982dd861c03a5fc1ac96a4af10fb479d05a5
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-27 16:28:27 -0400

    IMPEMENT TIME AND GAME OVER

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,26 +3,39 @@ const state = {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
-        score: document.querySelector("#score")
+        score: document.querySelector("#score"),
+        body: document.querySelector("body")
     },
     values: {
         timerId: null,
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        initialTime: 10,
     }
 };
 
 function randomSquare() {
-    state.view.squares.forEach((square)=>{
-        square.classList.remove("enemy");
-    });
+    if(state.values.initialTime > 0) {
+        state.view.squares.forEach((square)=>{
+            square.classList.remove("enemy");
+        });
 
-    let randomNumber = Math.floor(Math.random()*9);
+        let randomNumber = Math.floor(Math.random()*9);
 
-    let randomSquare = state.view.squares[randomNumber];
-    randomSquare.classList.add("enemy");
-    state.values.hitPosition = randomSquare.id;
+        let randomSquare = state.view.squares[randomNumber];
+        randomSquare.classList.add("enemy");
+        state.values.hitPosition = randomSquare.id;
+
+    } else {
+        playSound("gameover");
+        state.view.squares.forEach((square)=>{
+            square.classList.remove("enemy");
+            square.textContent = "GAME OVER";
+        });
+        clearInterval(state.values.timerId);
+        clearInterval(state.values.timeIntervalId);
+    }
 }
 
 function moveEnemy() {
@@ -42,14 +55,29 @@ function addListenerHitBox() {
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                state.values.initialTime++; // Aumentar o tempo quando há um clique bem-sucedido
+                state.view.timeLeft.textContent = state.values.initialTime;
                 playSound("hit");
             }
         });
     });
 }
 
+function decrementTime() {
+    state.values.timeIntervalId = setInterval(() => {
+        if (state.values.initialTime > 0) {
+            state.values.initialTime--;
+            state.view.timeLeft.textContent = state.values.initialTime;
+        } else {
+            clearInterval(state.values.timeIntervalId);
+        }
+    }, 1000); // Decrementa a cada segundo
+}
+
+
 function initialize() {
     moveEnemy();
+    decrementTime();
     addListenerHitBox();
 }
 
